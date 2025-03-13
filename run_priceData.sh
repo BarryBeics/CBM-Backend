a548b3ecbac0bfb1f525d191c449834598c93a0a
barry Marples <57953984+BarryBeics@users.noreply.github.com>
2025-03-13 20:46:44 +0000

    few additions

---
+++ run_priceData.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Running priceData at $(date)" >> /var/log/priceData.log
+/usr/local/bin/microservice-binaries/priceData --server=nats://nats:4222 --loglevel=debug --logcolor >> /var/log/priceData.log 2>&1
+echo "Finished priceData at $(date) with exit code $?" >> /var/log/priceData.log
+
+# Keep the container alive
+tail -f
