7920783bf8a9f17d6d86244ff16b8d4793707f3d
barry Marples <57953984+BarryBeics@users.noreply.github.com>
2025-03-17 20:07:26 +0000

    make into a binary

--- microservices/run_priceData.sh
+++ microservices/run_priceData.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Running priceData at $(date)" >> /var/log/priceData.log
-/usr/local/bin/microservice-binaries/priceData --server=nats://nats:4222 --loglevel=debug --logcolor >> /var/log/priceData.log 2>&1
+./usr/local/bin/microservice-binaries/priceData --server=nats://nats:4222 --loglevel=debug --logcolor >> /var/log/priceData.log 2>&1
 echo "Finished priceData at $(date) with exit code $?" >> /var/log/priceData.log
 
 # Keep the container alive
